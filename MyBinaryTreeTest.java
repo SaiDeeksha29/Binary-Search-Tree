9ecd6099008b2ff93f1a12c61f62f1ff2877aa56
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-12 16:13:23 +0530

    Added:All the integers are added and binary tree is made

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -1,21 +1,28 @@
 package com.blz.training;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
-
-import junit.framework.Assert;
 
 public class MyBinaryTreeTest {
 
 	@Test
 	public void createBinaryTreeAddIntegersTest() {
-		MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<Integer>();
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
-		int size=myBinaryTree.getSize();
-		Assert.assertEquals(3, size);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
 	}
-	
+
 }
