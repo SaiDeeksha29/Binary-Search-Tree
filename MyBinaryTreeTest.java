36d2b6c5d6963f030ef2de6833ba1cedd6e0a381
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-12 16:11:06 +0530

    Added:Created binary search tree and add three integers

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -8,6 +8,14 @@ import junit.framework.Assert;
 
 public class MyBinaryTreeTest {
 
-
+	@Test
+	public void createBinaryTreeAddIntegersTest() {
+		MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size=myBinaryTree.getSize();
+		Assert.assertEquals(3, size);
+	}
 	
 }
