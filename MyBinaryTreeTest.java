db41d3d9fd82261b009f25d4e669282983c63b18
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-12 16:17:31 +0530

    Added:Search the key in the binary tree

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -1,6 +1,7 @@
 package com.blz.training;
 
 import org.junit.Assert;
+
 import org.junit.Test;
 
 public class MyBinaryTreeTest {
@@ -23,6 +24,28 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(67);
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(13, size);
+	}
+
+	@Test
+	public void createBinaryTreeSearchIntegerTest() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
+		int key = myBinaryTree.search(63);
+		Assert.assertEquals(63, key);
 	}
 
 }
